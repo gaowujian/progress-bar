1c3fa9e6c0961be8acacbe05cb3979cde2a765a8
Tony <gwjlshebei@gmail.com>
2019-09-04 02:13:19 +1000

    write snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Title from './components/Title';
+import renderer from 'react-test-renderer';
+const http = require('http');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Title>demo</Title>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
